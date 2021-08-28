a369b220926eb89ff01a0c767cae3fbe4358a7fd
The-olajumoke <62016089+The-olajumoke@users.noreply.github.com>
2021-08-28 10:14:30 +0100

    fixing colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,13 +26,13 @@ function Header() {
         </div>
         <div className="grid grid-cols-2   lg:grid-cols-3  justify-around w-2/3 sm:flex md:w-full  items-center  sm:justify-end sm:p-5">
           <button
-            className=" border-2 mx-1 md:mx-2 text-primary border-solid text-sm md:text-lg  border-primary rounded-full  sm:px-12 py-1.5 hover:bg-primary hover:text-white"
+            className=" border-2 mx-1 md:mx-2 text-primary border-solid text-sm md:text-lg  border-border rounded-full  sm:px-12 py-1.5 hover:bg-border hover:text-white"
             onClick={openUserModal}
           >
             Sign Up
           </button>
           <button
-            className=" border-2 mx-1 md:mx-2 text-primary border-solid text-sm md:text-lg border-primary rounded-full sm:px-8 md:px-12 py-1.5 hover:bg-primary hover:text-white "
+            className=" border-2 mx-1 md:mx-2 text-primary border-solid text-sm md:text-lg border-border rounded-full sm:px-8 md:px-12 py-1.5 hover:bg-border hover:text-white "
             onClick={openLoginModal}
           >
             Log In
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -12,19 +12,19 @@ const Landing = () => {
   return (
     <div className=" h-auto sm:h-screen font-Poppins w-full flex flex-col items-center justify-between">
       <Header />
-      <div className="p-0 mb-28  px-0 sm:pl-6 flex flex-col sm:flex-row items-center mt-5 sm:mt-0 justify-between w-11/12  sm:w-full">
+      <div className="p-0 mb-28  px-0 sm:pl-6 flex flex-col sm:flex-row items-center mt-8 sm:mt-0 justify-between w-11/12  sm:w-full">
         <div className="sm:w-1/2  p-0 sm:px-4 ">
           <div className=" w-full md:5/6 lg:w-4/5  mb-4">
-            <div className="font-semibold leading-loose text-3xl md:text-4xl md:leading-relaxed lg:text-big lg:leading-tight sm:font-medium  mb-8 sm:mb-3">
+            <div className="font-semibold leading-relaxed text-3xl md:text-4xl md:leading-relaxed lg:text-big lg:leading-tight sm:font-medium  mb-4 sm:mb-3">
               <h1>Share ideas,</h1>
-              <h1> discover meaning</h1>
+              <h1> discover meaning.</h1>
             </div>
-            <h4 className=" md:5/6 lg:w-4/5 text-sm md:text-base leading-10 sm:leading-8 font-medium sm:font-normal text-textBody ">
+            <h4 className="w-4/5 md:5/6 text-md md:text-base leading-relaxed sm:leading-8 font-medium sm:font-normal text-textBody ">
               Education, presentations, collaboration. Now, you can interact
               easily and seamlessly.
             </h4>
           </div>
-          <div className="w-full sm:mt-12 flex flex-col mb-7  sm:flex-row sm:mb-0 ">
+          <div className="w-full mt-12 flex flex-col mb-7  sm:flex-row sm:mb-0 ">
             <div className="grid grid-cols-1 xl:grid-cols-2 sm:gap-4 w-full justify-between sm:w-9/12 pl-0 p-2 items-center">
               <button className=" relative flex items-center   justify-evenly  text-center border-primary  bg-primary p-3 rounded-full mb-5 text-white  w-3/4  sm:w-full sm:mb-0 md:text-base lg:text-xl ">
                 <FiMessageSquare className="button" />
@@ -34,7 +34,7 @@ const Landing = () => {
                 type="text"
                 value={value}
                 onChange={handleKey}
-                className="border border-border p-3 rounded-full outline-none w-full md:text-base"
+                className="border-2 border-border p-3 rounded-full outline-none w-full md:text-base"
                 placeholder="Enter Inso link"
               />
             </div>
