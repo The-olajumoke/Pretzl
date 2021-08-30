b5b795f76c412f92b9ed0dd888352b13779ab1f2
The-olajumoke <62016089+The-olajumoke@users.noreply.github.com>
2021-08-30 10:05:26 +0100

    form

--- src/components/SignUp/FormInput.js
+++ src/components/SignUp/FormInput.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function FormInput({label,name,type,placeholder}) {
+function FormInput({label,name,type,placeholder,loginBtn, setLoginBtn}) {
+const [value, setValue] = useState('')
+
+  const handleOnClick=(e)=>{
+setValue(e.target.value)
+if (e.target.value.length>0 ) {
+  setLoginBtn(true)
+  
+} else {
+  setLoginBtn(false)
+}
+  }
+ 
     return (
       <div className="h-20 mb-5 sm:mb-8 flex flex-col">
         <label className="text-primary" htmlFor={label}>{label} <span style={{color:"red"}}>*</span> </label>
         <input className="mt-1 p-3 rounded-md outline-none h-16 border border-border bg-white text-black" name={name} type={type}
+     onChange={handleOnClick}
+      value={value}
        placeholder={placeholder} required  />
       </div>
     );
